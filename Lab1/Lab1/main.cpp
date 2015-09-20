197c3231b85cc73a8b2072c6e06731a64979afb0
RoadToExclusivity <dimaoq@gmail.com>
2015-09-20 12:38:27 +0300

    init

---
+++ Lab1/Lab1/main.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+bool IsCorrectDecl(const string &line)
+{
+	string declString = "", idString = "", typeAssignString = "", typeString = "";
+
+	istringstream iss(line);
+	getline(iss, declString, ' ');
+	getline(iss, idString, ' ');
+	getline(iss, typeAssignString, ' ');
+	getline(iss, typeString, ' ');
+
+	return (declString == "VAR" && idString == "IDENTIFIER" && typeAssignString == "COLON" &&
+		(typeString == "INTEGER" || typeString == "STRING" || typeString == "BOOLEAN" || typeString == "DOUBLE"));
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+	{
+		cout << "Not enough arguments" << endl;
+		return 0;
+	}
+
+	ifstream fin(argv[1]);
+	if (!fin)
+	{
+		cout << "No such file" << endl;
+		return 0;
+	}
+
+	ofstream fout("result.txt");
+	string line;
+	while (getline(fin, line))
+	{
+		if (line != "")
+		{
+			fout << line << " - ";
+			if (IsCorrectDecl(line))
+			{
+				fout << "correct";
+			}
+			else
+			{
+				fout <<	"incorrect";
+			}
+
+			fout << endl;
+		}
+	}
+
+	return 0;
+}
